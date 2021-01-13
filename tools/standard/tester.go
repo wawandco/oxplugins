f6127c80fe9ec5c8250d619622014c3dbf8068c3
Antonio Pagano <apagano@wawand.co>
2021-01-13 15:42:31 -0500

    adding -p 1 when not specified

--- tools/standard/tester.go
+++ tools/standard/tester.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 type Tester struct{}
@@ -19,20 +20,30 @@ func (b *Tester) RunBeforeTest(ctx context.Context, root string, args []string) 
 
 func (p *Tester) Test(ctx context.Context, root string, args []string) error {
 	fmt.Println(">>> Running Tests")
-	cargs := []string{
-		"test",
-	}
 
-	if len(args) > 0 {
-		cargs = append(cargs, args...)
-	} else {
-		cargs = append(cargs, "./...", "-p", "1")
-	}
-
-	cmd := exec.CommandContext(ctx, "go", cargs...)
+	cmd := exec.CommandContext(ctx, "go", p.testArgs(args)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
+	fmt.Println(cmd.Args)
+
 	return cmd.Run()
+}
+
+func (p *Tester) testArgs(args []string) []string {
+	base := []string{
+		"test",
+	}
+
+	if !strings.Contains(strings.Join(args, " "), "-p") {
+		base = append(base, "-p", "1")
+	}
+
+	cargs := append(base, "./...")
+	if len(args) > 0 {
+		cargs = append(base, args...)
+	}
+
+	return cargs
 }
