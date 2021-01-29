ecb15f5567fd2172ab54a80c9ce9b820e62196a6
Antonio Pagano <apagano@wawand.co>
2021-01-29 12:55:36 -0500

    adding some info about the force option.

--- tools/buffalo/folders/initializer.go
+++ tools/buffalo/folders/initializer.go
@@ -42,6 +42,8 @@ var (
 // - `ox new bongo` 					=> creates the new app in the bongo folder
 // - `ox new github.com/wawandco/bongo` => creates the new app in the bongo folder
 // - `ox new wawandco/bongo` 			=> creates the new app in the bongo folder
+// It will fail if the existing folder exists, if you want to replace existing
+// folder use the --force option.
 type Initializer struct {
 	// force folder creation if exists.
 	force bool
