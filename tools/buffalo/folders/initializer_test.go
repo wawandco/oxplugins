4984ce1a1ef51da1c7a11c7f0099252b801edda2
Antonio Pagano <apagano@wawand.co>
2021-01-29 09:48:30 -0500

    adding some tests

--- tools/buffalo/folders/initializer_test.go
+++ tools/buffalo/folders/initializer_test.go
@@ -1,7 +1,44 @@
 package folders
 
-import "testing"
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
 
 func TestInitializerRun(t *testing.T) {
+
+	t.Run("valid args", func(t *testing.T) {
+		root := t.TempDir()
+		err := os.Chdir(root)
+		if err != nil {
+			t.Fatal("could not move to temp dir")
+		}
+
+		i := Initializer{}
+		err = i.Initialize(context.Background(), root, []string{"new", "app"})
+		if err != nil {
+			t.Errorf("err should be nil but got %v", err)
+		}
+
+		for _, v := range folders {
+			v = strings.ReplaceAll(v, "[name]", "app")
+			v = filepath.Join(root, v)
+
+			if _, err := os.Stat(v); os.IsNotExist(err) {
+				t.Errorf("should have created %v", v)
+			}
+		}
+	})
+
+	t.Run("invalid args", func(t *testing.T) {
+		i := Initializer{}
+		err := i.Initialize(context.Background(), "", []string{"new"})
+		if err == nil {
+			t.Errorf("err should not be nil but got nil")
+		}
+	})
 
 }
