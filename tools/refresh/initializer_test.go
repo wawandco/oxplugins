4590b26a003ccb9353dfb179f2e305d0c279ea42
Antonio Pagano <apagano@wawand.co>
2021-02-03 13:53:26 -0500

    checking that error

--- tools/refresh/initializer_test.go
+++ tools/refresh/initializer_test.go
@@ -19,7 +19,10 @@ func TestInitializer(t *testing.T) {
 			t.Error("could not change to temp directory")
 		}
 
-		os.MkdirAll(filepath.Join(root, "myapp"), 0777)
+		err = os.MkdirAll(filepath.Join(root, "myapp"), 0777)
+		if err != nil {
+			t.Error("could not change to temp directory")
+		}
 
 		i := Initializer{}
 		var dx sync.Map
@@ -56,7 +59,11 @@ func TestInitializer(t *testing.T) {
 			t.Error("could not change to temp directory")
 		}
 
-		os.MkdirAll(filepath.Join(root, "myapp"), 0777)
+		err = os.MkdirAll(filepath.Join(root, "myapp"), 0777)
+		if err != nil {
+			t.Fatal("could not create dir")
+		}
+
 		rootYml := filepath.Join(root, "myapp", ".buffalo.dev.yml")
 		_, err = os.Create(rootYml)
 		if err != nil {
